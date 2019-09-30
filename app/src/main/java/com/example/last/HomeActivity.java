879a07bbf6b1f969068eaecff0517e16f917021a
YoonSuHyeon <96-05-03@naver.com>
2019-09-30 21:35:08 +0900

    2019-09-30 윤수현

--- app/src/main/java/com/example/last/HomeActivity.java
+++ app/src/main/java/com/example/last/HomeActivity.java
@@ -2,13 +2,26 @@ package com.example.last;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class HomeActivity extends AppCompatActivity {
-
+    LinearLayout li_test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        li_test=findViewById(R.id.li_test);
+
+        li_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent test = new Intent(HomeActivity.this, TestActivity.class);
+                startActivity(test);
+            }
+        });
     }
 }
