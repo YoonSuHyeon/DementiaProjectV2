729c950055b65aa601352ba798f79c2533b6520b
YoonSuHyeon <96-05-03@naver.com>
2019-11-05 20:40:04 +0900

    #V2 TrainingResultActivity(2)

--- app/src/main/java/com/example/last/TrainingResultActivity.java
+++ app/src/main/java/com/example/last/TrainingResultActivity.java
@@ -10,18 +10,18 @@ public class TrainingResultActivity extends AppCompatActivity {
     Intent intent;
     int score;
     String uid;
-    TextView trainingTextView;
+    TextView scoreTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_training_result);
-        trainingTextView=findViewById(R.id.score);
+        scoreTextView=findViewById(R.id.score);
 
 
         intent = getIntent();
         score = intent.getIntExtra("score",0); //trainingTestActivity 에서 보낸 인텐트 score 의 값을 가져온다.
         uid = intent.getStringExtra("uid"); //사용자의 정보를 가져오기 위해서는 uid르 사용해서 Firebase에 접근한다.
-        trainingTextView.setText("30점 만점"+score+"입니다.");
+        scoreTextView.setText("30점 만점 중 "+score+"점입니다.");
 
     }
 }
--- app/src/main/java/com/example/last/TrainingTestActivity.java
+++ app/src/main/java/com/example/last/TrainingTestActivity.java
@@ -2,13 +2,30 @@ package com.example.last;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TrainingTestActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_training_test);
+        button = findViewById(R.id.trainingbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent training = new Intent(TrainingTestActivity.this, TrainingResultActivity.class);
+
+                    int score =0 ;
+                    String uid = getIntent().getStringExtra("uid");
+                    training.putExtra("uid", uid);
+                    training.putExtra("score", score);
+
+                    startActivity(training);
+            }
+        });
     }
 }
