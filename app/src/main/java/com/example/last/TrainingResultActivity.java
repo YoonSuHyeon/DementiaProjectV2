df0de630e4c144da523ca8d9ef45027b2259f5e0
bdh0702 <bdh0702@naver.com>
2019-11-05 20:35:31 +0900

    #tranning_result XML UI 수정(3)

--- app/src/main/java/com/example/last/TrainingResultActivity.java
+++ app/src/main/java/com/example/last/TrainingResultActivity.java
@@ -15,7 +15,7 @@ public class TrainingResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_training_result);
-        trainingTextView=findViewById(R.id.trainingTextView);
+        trainingTextView=findViewById(R.id.score);
 
 
         intent = getIntent();
